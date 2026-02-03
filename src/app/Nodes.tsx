8ee253af4196a8b81b1f859924aa5e1fbedbd0bb
majkus522 <mikohus522@gmail.com>
2026-02-03 22:50:51 +0100

    Merge 2 nodes

--- src/app/Nodes.tsx
+++ src/app/Nodes.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 type ItemNodeProps = {
 	item: string;
 	ratio: number;
@@ -16,26 +14,23 @@ type MachineNodeProps = {
 
 export function ItemNode({item, ratio, x = 0, y = 0}: ItemNodeProps)
 {
-	return (
-		<g className="factoryNode" transform={`translate(${x}, ${y})`} width="200" height="200">
-			<rect width="100" height="100" x="0" y="0" fill="rgb(69,69,69)" ry="5" rx="5"></rect>
-			<rect width="100" height="30" x="0" y="70" fill="rgb(245,245,245)" ry="5" rx="5"></rect>
-			<image href={`images/items/${item}.webp`} alt={item} width="70" height="70" x="15" y="0"/>
-			<text x="50" y="82.5" text-anchor="middle">{item}</text>
-			<text x="50" y="95.5" text-anchor="middle">{ratio} / min</text>
-		</g>
-	);
+	return BasicNode(`images/items/${item}.webp`, item, ratio + " / min", x, y);
 }
 
 export function MachineNode({machine, usage, x = 0, y = 0}: MachineNodeProps)
+{
+	return BasicNode(`images/machines/${machine}.webp`, machine, String(usage), x, y);
+}
+
+function BasicNode(image: string, name: string, desc: string, x: number, y: number)
 {
 	return (
 		<g className="factoryNode" transform={`translate(${x}, ${y})`} width="200" height="200">
 			<rect width="100" height="100" x="0" y="0" fill="rgb(69,69,69)" ry="5" rx="5"></rect>
 			<rect width="100" height="30" x="0" y="70" fill="rgb(245,245,245)" ry="5" rx="5"></rect>
-			<image href={`images/machines/${machine}.webp`} alt={machine} width="70" height="70" x="15" y="0"/>
-			<text x="50" y="82.5" text-anchor="middle">{machine}</text>
-			<text x="50" y="95.5" text-anchor="middle">{usage}</text>
+			<image href={image} width="70" height="70" x="15" y="0"/>
+			<text x="50" y="82.5" text-anchor="middle">{name}</text>
+			<text x="50" y="95.5" text-anchor="middle">{desc}</text>
 		</g>
 	);
 }
