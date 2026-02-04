603a1337f7160e4ef47792a8eb5d8f8d4c40f5d0
majkus522 <mikohus522@gmail.com>
2026-02-04 15:07:20 +0100

    Branching display

--- src/app/common.ts
+++ src/app/common.ts
@@ -12,13 +12,32 @@ export type Recipe = {
 	machine: string;
 };
 
+export class Vector2
+{
+	public x: number;
+	public y: number;
+
+	public constructor(x: number, y: number)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	public step(count: number, index: number): Vector2
+	{
+		if (count >= 2)
+			return new Vector2(this.x - 300, this.y + (index == 0 ? 150 : -150));
+		return new Vector2(this.x - 300, this.y);
+	}
+}
+
 export class Chain
 {
 	public head: ChainLink;
 
 	public constructor(item: string, amount: number)
 	{
-		this.head = new ChainLink(item, amount);
+		this.head = new ChainLink(item, amount, new Vector2(1400, 0));
 	}
 }
 
@@ -27,34 +46,51 @@ export class ChainLink
 	public item: string;
 	public amount: number;
 	public usage: number = 0;
-	recipe: Recipe;
+	public position: Vector2;
+	public machine: string = "";
 	left?: ChainLink | undefined;
 	right?: ChainLink | undefined;
+	movedAlready: boolean = false;
 
-	public constructor(item: string, amount: number)
+	public constructor(item: string, amount: number, position: Vector2)
 	{
 		this.item = item;
 		this.amount = amount;
+		this.position = position;
 	}
 
-	public async eval()
+	public async eval(parent: ChainLink | undefined)
 	{
 		const recipes: Recipe[] = await getRecipes(this.item);
 		if (recipes.length == 0) return;
-		this.recipe = recipes[0];
-		this.usage = ((this.amount / this.recipe.output.amount) * this.recipe.time) / 60;
-		if (this.recipe.inputs != null)
+		const recipe: Recipe = recipes[0];
+		this.machine = recipe.machine;
+		this.usage = ((this.amount / recipe.output.amount) * recipe.time) / 60;
+		if (recipe.inputs != null)
 		{
-			if (this.recipe.inputs[0] != null)
+			if (recipe.inputs.length >= 2 && this.position.y != 0)
 			{
-				this.left = new ChainLink(this.recipe.inputs[0].item, (this.amount / this.recipe.output.amount) * this.recipe.inputs[0].amount,);
-				await this.left.eval();
+				this.position.y += (this.position.y > 0 ? 150 : -150);
+				console.log(this.item);
+				await parent?.moveUp();
 			}
-			if (this.recipe.inputs[1] != null)
+			if (recipe.inputs[0] != null)
 			{
-				this.right = new ChainLink(this.recipe.inputs[1].item, (this.amount / this.recipe.output.amount) * this.recipe.inputs[1].amount,);
-				await this.right.eval();
+				this.left = new ChainLink(recipe.inputs[0].item, (this.amount / recipe.output.amount) * recipe.inputs[0].amount, this.position.step(recipe.inputs.length, 0));
+				await this.left.eval(this);
+			}
+			if (recipe.inputs[1] != null)
+			{
+				this.right = new ChainLink(recipe.inputs[1].item, (this.amount / recipe.output.amount) * recipe.inputs[1].amount, this.position.step(recipe.inputs.length, 1));
+				await this.right.eval(this);
 			}
 		}
+	}
+
+	public async moveUp()
+	{
+		if (this.position.y != 0)
+			this.position.y += ((this.position.y > 0 ? 150 : -150) * (this.movedAlready ? 0.5 : 1));
+		this.movedAlready = true;
 	}
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,25 @@ import {JSX, useState} from "react";
 import {ItemNode, MachineNode} from "@/app/Nodes";
 import {Chain, ChainLink} from "@/app/common";
 
+let items: JSX.Element[] = [];
+let index = 0;
+
+function display(node: ChainLink | undefined)
+{
+	if (node == null)
+		return;
+	items.push(<ItemNode item={node.item} ratio={node.amount} x={node.position.x} y={node.position.y} key={index} />);
+	items.push(<MachineNode machine={node.machine} usage={node.usage} x={node.position.x - 150} y={node.position.y} key={index + 1} />);
+	index += 2;
+	display(node.left);
+	index += 2;
+	display(node.right);
+}
+
 export default function Home()
 {
 	const [chain, setChain] = useState<Chain>();
-
-	const items: JSX.Element[] = [];
-	let node: ChainLink | undefined = chain?.head;
-	let x = 1000;
-	while (node != null)
-	{
-		items.push(<ItemNode item={node.item} ratio={node.amount} x={x} key={`item-${node.item}-${x}`} />);
-		items.push(<MachineNode machine={node.recipe.machine} usage={node.usage} x={x - 150} key={`machine-${node.recipe.machine}-${x}`} />);
-		x -= 300;
-		node = node.left;
-	}
+	display(chain?.head);
 
 	return (
 		<main>
@@ -27,13 +32,15 @@ export default function Home()
 					let chain = new Chain("cryston-powder", 30);
 					await chain.head.eval();
 					setChain(chain);
+					while(items.length > 0)
+						items.pop();
 				}}/>
 			</section>
 			<section>
 				{JSON.stringify(chain)}
 			</section>
 			<section>
-				<svg viewBox="0 0 1500 700" xmlns="http://www.w3.org/2000/svg">
+				<svg viewBox="0 -700 1500 1400" xmlns="http://www.w3.org/2000/svg">
 					{items}
 				</svg>
 			</section>
